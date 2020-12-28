9b5cceed20a5121ae380caad5a4d9dc96e7b2453
lexifdev <sl@lxf.kr>
2020-12-28 19:06:59 +0900

    login 안되어있으면 login 화면으로 바로 보냄

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { signIn, useSession } from 'next-auth/client'
+import { useSession, getSession } from 'next-auth/client'
 
 import ChannelList from 'components/ChannelList'
 import MessageList from 'components/MessageList'
@@ -9,22 +9,35 @@ import style from './index.styl'
 
 
 const Index = () => {
-  const [ session, loading ] = useSession()
+  const [ session ] = useSession()
   const { channel } = SlackStore.useState(s => s)
+
+  if (!session) {
+    return null
+  }
 
   return (
     <div className={style.app}>
-      {session ?
-        <>
-          <ChannelList />
-          <MessageList channel={channel} />
-        </> :
-        <div>
-          <button onClick={signIn}>Sign in</button>
-        </div>
-      }
+      <ChannelList />
+      <MessageList channel={channel} />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+  const { res } = context
+
+  if (!session) {
+    res.writeHeader(307, {
+      Location: '/api/auth/signin'
+    })
+    res.end()
+  }
+
+  return {
+    props: { session }
+  }
 }
 
 export default Index
